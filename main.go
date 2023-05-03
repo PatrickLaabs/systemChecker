44d35e3d81ad2c814c7a8fd1d87f57973bc3326d
PatrickLaabs <patrick.laabs@me.com>
2023-05-03 14:28:28 +0200

    updates

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"os"
 	"text/template"
 	"time"
 
@@ -17,8 +18,19 @@ type Config struct {
 	Dependencies map[string][]string `toml:"dependencies"`
 }
 
+func (c *Config) LoadFromFile(path string) error {
+	_, err := toml.DecodeFile(path, c)
+	if err != nil {
+		log.Printf("Error decoding file: %s", err.Error())
+		return err
+	}
+	log.Printf("Successfully decoded file: %s", path)
+	return nil
+}
+
 func main() {
 	var config Config
+	log.Printf("config: %+v\n", config)
 	if _, err := toml.DecodeFile("config.toml", &config); err != nil {
 		log.Fatal(err)
 	}
@@ -36,7 +48,12 @@ func main() {
 	r := gin.Default()
 
 	r.SetFuncMap(template.FuncMap{
-		"ternary": ternary,
+		"ternary": func(b bool, t, f interface{}) interface{} {
+			if b {
+				return t
+			}
+			return f
+		},
 	})
 
 	r.GET("/", func(c *gin.Context) {
@@ -58,28 +75,33 @@ func main() {
 		})
 	})
 
-	r.Static("/static", "./static")
+	r.Static("/static", "static")
 
 	r.LoadHTMLGlob("templates/*.tmpl")
 
 	if err := r.Run(":8080"); err != nil {
 		fmt.Println("Error starting server:", err)
+		os.Exit(1)
 	}
 }
 
 func CheckVMStatus(address string, timeout time.Duration) string {
+	log.Printf("Checking VM status for %s", address)
 	conn, err := net.DialTimeout("tcp", address+":22", timeout)
 	if err != nil {
 		log.Printf("Error checking VM Status for %s: %s", address, err)
 		return "offline"
 	}
 	defer conn.Close()
+	log.Printf("VM %s is online", address)
 	return "online"
 }
 
 func ternary(condition bool, trueValue, falseValue interface{}) interface{} {
 	if condition {
+		log.Printf("Condition was true, returning %v", trueValue)
 		return trueValue
 	}
+	log.Printf("Condition was false, returning %v", falseValue)
 	return falseValue
 }
